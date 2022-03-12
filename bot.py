9f984cdfa8e74fbe49b65fb02a8a517980f43f3c
ArthThapa <arththapa7@gmail.com>
2022-03-12 18:42:43 +0530

    ✅setup coworking cog

---
+++ bot.py
@@ -0,0 +1,25 @@
+from os import environ, listdir
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = environ['DISCORD_TOKEN']
+
+bot = commands.Bot(command_prefix='>', help_command=None)
+
+@bot.event
+async def on_ready():
+  print("====================")
+  print("self.dev Watcher is ready")
+  print("====================")
+
+for filename in listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+for filename in listdir('./events'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'events.{filename[:-3]}')
+
+bot.run(token)
---
+++ events/coworking.py
@@ -0,0 +1,33 @@
+from os import environ
+from discord.ext import commands
+from json import loads
+
+role_id = int(environ['COWORKER_ROLE_ID'])
+channel_ids = loads(environ['COWORKING_CHANNELS_IDS'])
+fallout_channel_id = int(environ['FALLOUT_CHANNEL_ID'])
+
+class coworking(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    print('init')
+    
+  @commands.Cog.listener()
+  async def on_voice_state_update(self, member, before, after):
+    
+    try:
+      guild = self.bot.get_guild(member.guild.id)
+      role = guild.get_role(role_id)
+      before_channel = before.channel
+      after_channel = after.channel
+
+      if after_channel and after_channel.id in channel_ids:
+        await member.add_roles(role, reason='Joined Co-working vc')
+
+      elif before_channel and before_channel.id in channel_ids:
+        await member.remove_roles(role, reason='Left Co-working vc')
+    except AttributeError:
+      fallout_channel = member.guild.get_channel(fallout_channel_id)
+      await fallout_channel.send(f"Coworker role with id `{role_id}` doesn't exist! Please update env variables in production :pray:")
+
+def setup(bot):
+  bot.add_cog(coworking(bot))
